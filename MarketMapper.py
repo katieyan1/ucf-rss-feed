35eee76bfe9fb9ac263909993b66d0337b06eb4d
katie yan <katieyan@utexas.edu>
2026-04-20 15:10:31 -0500

    remove starter code

--- MarketMapper.py
+++
@@ -1,8 +0,0 @@
-from helper_classes import Story, Market
-
-class MarketMapper:
-    def __init__(self):
-        self.markets : list[Market] = [] # markets to track
-
-    def get_markets(self, headline: Story) -> list[Market]:
-        pass
